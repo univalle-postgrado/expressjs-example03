9d3a8337addf6fec8d958d3b0d9a01f3d4fd19ff
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-08-16 11:00:30 -0400

    conexión a base de datos

--- app.js
+++ app.js
@@ -1,88 +1,110 @@
 const express = require('express');
+const { Client } = require('pg');
 const { v4: uuidv4 } = require('uuid');
 const { body, validationResult } = require('express-validator');
 const app = express();
 const port = 3000;
 
-let movies = [
-    { id: '619971fa-a379-4736-a69e-8fa8091376e7', title: 'El Padrino', year: 1980 },
-    { id: 'd9f2aa8a-1e88-4986-939a-09d8e4ce9767', title: 'El Señor de los Anillos', year: 2002 }
-];
-
 app.use(express.json());
 
+const db = new Client({
+  user: 'univalle',
+  host: 'localhost',
+  database: 'univalle_dbmovies',
+  password: '123456',
+  port: 5432
+});
+db.connect();
+
 const validateMovie = [
-    body('title').notEmpty().withMessage('El título es requerido'),
+  body('title').notEmpty().withMessage('El título es requerido'),
 ];
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo!');
 });
 
-app.get('/movies', (req, res) => {
-  res.json(movies);
+app.get('/movies', async (req, res) => {
+  const { rows } = await db.query("SELECT * FROM movies");
+  res.json(rows);
 });
-app.get('/movies/:id', (req, res) => {
-  const movie = movies.find(m => m.id === req.params.id);
-  if (movie) {
-    res.json(movie);
+app.get('/movies/:id', async (req, res) => {
+  const { rows, rowCount } = await db.query("SELECT * FROM movies WHERE id=" + req.params.id);
+  if (rowCount > 0) {
+    res.json(rows[0]);
   } else {
     res.status(404).json({ message: 'Película no encontrada' });
   }
 });
-app.post('/movies', validateMovie, (req, res) => {
-    const errors = validationResult(req, res);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ error: errors.array() });
+app.post('/movies', validateMovie, async (req, res) => {
+  const errors = validationResult(req, res);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() });
+  }
+
+  const { rows } = await db.query('INSERT INTO movies (title, year) VALUES ($1, $2) RETURNING *', [req.body.title, req.body.year]);
+  res.status(201).json(rows[0]);
+});
+app.put('/movies/:id', validateMovie, async (req, res) => {
+  const errors = validationResult(req, res);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() });
+  }
+
+  const { title, year } = req.body;
+  const { rowCount } = await db.query(`SELECT * FROM movies WHERE id=${req.params.id}`);
+  if (rowCount > 0) {
+    const { rows } = await db.query(`UPDATE movies SET title = '${title}', year = '${year}' WHERE id = '${req.params.id}' RETURNING *`);
+    res.json(rows[0]);
+  } else {
+    const { rows } = await db.query('INSERT INTO movies (title, year) VALUES ($1, $2) RETURNING *', [req.body.title, req.body.year]);
+    res.status(201).json(rows[0]);
+  }
+});
+app.patch('/movies/:id', async (req, res) => {
+  const id = req.params.id;
+  const { rowCount } = await db.query(`SELECT * FROM movies WHERE id=${id}`);
+  if (rowCount == 0) {
+    res.status(404).json({ message: 'Película no encontrada' });
+  } else {
+    const { title, year } = req.body;
+    let updateQuery = 'UPDATE movies SET ';
+    let updateValues = [];
+
+    let i = 1;
+    if (title) {
+      updateQuery += `title = $${i}, `;
+      updateValues.push(title);
+      i++;
     }
 
-    const newMovie = {
-        ...req.body,
-        id: uuidv4()
+    if (year) {
+      updateQuery += `year = $${i}, `;
+      updateValues.push(year);
+      i++;
     }
-    movies.push(newMovie);
-    res.status(201).json(newMovie);
-});
-app.put('/movies/:id', validateMovie, (req, res) => {
-    const errors = validationResult(req, res);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ error: errors.array() });
-    }
-        
-    const id = req.params.id;
-    const movieIndex = movies.findIndex(m => m.id === id);
-    if (movieIndex !== -1) {
-        movies[movieIndex] = { ...req.body, id };
-        res.json(movies[movieIndex]);
+
+    updateQuery = updateQuery.slice(0, -2); // Quitar la coma final
+    updateQuery += ' WHERE id = $' + i + ' RETURNING *';
+    updateValues.push(id); 
+
+    const { rows } = await db.query(updateQuery, updateValues);
+    if (rows.length > 0) {
+      res.json(rows[0]);
     } else {
-        const newMovie = {
-            ...req.body,
-            id: id
-        }
-        lastId = id;
-        movies.push(newMovie);
-        res.status(201).json(newMovie);
+      res.status(404).json({ message: 'No se pudo actualizar la película' })
     }
+  }
 });
-app.patch('/movies/:id', (req, res) => {
-    const id = req.params.id;
-    const movieIndex = movies.findIndex(m => m.id === id);
-    if (movieIndex !== -1) {
-        Object.assign(movies[movieIndex], req.body);
-        res.json(movies[movieIndex]);
-    } else {
-        res.status(404).json({ message: 'Película no encontrada' })
-    }
-});
-app.delete('/movies/:id', (req, res) => {
-    const id = req.params.id;
-    const movieIndex = movies.findIndex(m => m.id === id);
-    if (movieIndex !== -1) {
-        movies.splice(movieIndex, 1);
-        res.sendStatus(204);
-    } else {
-        res.status(404).json({ message: 'Película no encontrada' })
-    }
+app.delete('/movies/:id', async (req, res) => {
+  const id = req.params.id;
+  const { rowCount } = await db.query(`SELECT * FROM movies WHERE id=${id}`);
+  if (rowCount == 0) {
+    res.status(404).json({ message: 'Película no encontrada' });
+  } else {
+    await db.query('DELETE FROM movies WHERE id = $1', [id]);
+    res.sendStatus(204);
+  }
 });
 
 app.listen(port, () => {
---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE movies (
+    id serial PRIMARY KEY,
+    title character varying(150) NOT NULL,
+    year integer
+);
